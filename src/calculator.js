2ce81ccea78d204662bf210f3ea845a343423cb7
asritha reddy <asrithareddy@asrithas-MacBook-Air.local>
2023-10-16 12:21:13 +0530

    first commit

---
+++ src/calculator.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+function Calculator() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input));
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" value={input} />
+      <div>
+        <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+      </div>
+      <div>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+      </div>
+      <div>
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
+      </div>
+      <div>
+        <button onClick={() => handleButtonClick('C')}>C</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button onClick={() => handleButtonClick('=')}>=</button>
+        <button onClick={() => handleButtonClick('/')}>/</button>
+      </div>
+      <p>Result: {result}</p>
+    </div>
+  );
+}
+
+export default Calculator;
